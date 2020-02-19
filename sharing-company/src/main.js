47cdadf0a0a571f9e1df7e59e8c19dd5f6586f0e
リョウタ <haramatsuryota@gmail.com>
2020-02-20 00:36:59 +0800

    clear cache

--- sharing-company/src/main.js
+++ sharing-company/src/main.js
@@ -9,28 +9,15 @@ import router from './router.js'
 import { firestorePlugin } from 'vuefire'
 import './assets/main.css'
 import vuetify from './plugins/vuetify';
+import firebaseConfig from './firebase-config'
 
-
-
-var firebaseConfig = {
-    apiKey: "AIzaSyAvgkc0u_M1kEubRGSgniE5OTmSbTW0jrQ",
-    authDomain: "share-company-13b31.firebaseapp.com",
-    databaseURL: "https://share-company-13b31.firebaseio.com",
-    projectId: "share-company-13b31",
-    storageBucket: "share-company-13b31.appspot.com",
-    messagingSenderId: "144249106976",
-    appId: "1:144249106976:web:f76a87ce17639337970245",
-    measurementId: "G-SN817R9J87"
-  };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 Vue.config.productionTip = false
 
 Vue.use(firestorePlugin)
 
-
-export const db = firebase.firestore()
 export const auth = firebase.auth()
+export const db = firebase.firestore()
 export const storage = firebase.storage();
 
 
